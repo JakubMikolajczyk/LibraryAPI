b7e77772df86bfd73a85cf7d4b431adef6497733
Jakub Mikolajczyk <Mikolajczyk.Jakub@outlook.com>
2023-10-11 01:11:51 +0200

    perf(AuthorController)

--- src/main/java/com/Library/restAPI/controller/AuthorController.java
+++ src/main/java/com/Library/restAPI/controller/AuthorController.java
@@ -12,33 +12,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/authors")
+@RequestMapping("/api/v1/")
 public class AuthorController {
 
     private final AuthorService authorService;
     private final AuthorMapper authorMapper;
 
-    @GetMapping("/{id}")
+    @GetMapping("authors/{id}")
     public AuthorDto getAuthor(@PathVariable Long id){
         return AuthorMapper.toDto(authorService.getAuthorById(id));
     }
 
-    @GetMapping
+    @GetMapping("authors")
     public Page<AuthorDto> getAllAuthors(@PageableDefault(sort = "id", value = 50) Pageable pageable){
         return authorService.getAll(pageable).map(AuthorMapper::toDto);
     }
     
-    @PostMapping
+    @PostMapping("authors")
     public void createAuthor(@RequestBody AuthorRequest authorRequest){
         authorService.createAuthor(authorMapper.toEntity(authorRequest));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("authors/{id}")
     public void editAuthor(@PathVariable Long id, @RequestBody AuthorRequest authorRequest){
         authorService.editAuthor(authorMapper.toEntity(id, authorRequest));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("authors/{id}")
     public void deleteAuthor(@PathVariable Long id){
         authorService.deleteAuthor(id);
     }
