d501e8e1d1478bb0c418151d7bad59279d15a56f
Jakub Mikolajczyk <Mikolajczyk.Jakub@outlook.com>
2023-10-16 13:17:59 +0200

    refactor(UserController): extract from UserController user endpoints ("users/me") to UserMeController

--- src/main/java/com/Library/restAPI/controller/UserController.java
+++ src/main/java/com/Library/restAPI/controller/UserController.java
@@ -1,20 +1,15 @@
 package com.Library.restAPI.controller;
 
-import com.Library.restAPI.dto.request.PasswordChangeRequest;
 import com.Library.restAPI.dto.request.UserEditAdminRequest;
-import com.Library.restAPI.dto.request.UserEditRequest;
 import com.Library.restAPI.dto.response.BorrowDto;
 import com.Library.restAPI.dto.response.BorrowHistoryDto;
 import com.Library.restAPI.dto.response.UserDto;
 import com.Library.restAPI.mapper.BorrowHistoryMapper;
 import com.Library.restAPI.mapper.BorrowMapper;
 import com.Library.restAPI.mapper.UserMapper;
-import com.Library.restAPI.security.UsernameAndIdPrincipal;
-import com.Library.restAPI.service.AuthService;
 import com.Library.restAPI.service.BorrowHistoryService;
 import com.Library.restAPI.service.BorrowService;
 import com.Library.restAPI.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,58 +24,9 @@ public class UserController {
     private final UserService userService;
     private final BorrowService borrowService;
     private final BorrowHistoryService borrowHistoryService;
-    private final AuthService authService;
     private final UserMapper userMapper;
     private final BorrowMapper borrowMapper;
 
-    @GetMapping("users/me")
-    public UserDto getMe(){
-        return userMapper.toDto(userService.getUserById(UsernameAndIdPrincipal.getIdFromSecurityCtx()));
-    }
-
-    @PutMapping("users/me")
-    public void editMe(@RequestBody UserEditRequest userEditRequest){
-        userService.updateUser(userMapper.toEntity(UsernameAndIdPrincipal.getIdFromSecurityCtx(), userEditRequest));
-    }
-
-    @GetMapping("users/me/borrows")
-    public List<BorrowDto> getMyBorrows(){
-        return borrowService
-                .getAllBorrowsByUserId(UsernameAndIdPrincipal.getIdFromSecurityCtx())
-                .stream()
-                .map(borrowMapper::toDto)
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping("users/me/borrow-histories")
-    public List<BorrowHistoryDto> getMyHistory(@RequestParam(required = false) boolean showHidden){
-        return borrowHistoryService
-                .getAllHistoryByUserId(UsernameAndIdPrincipal.getIdFromSecurityCtx(),showHidden)
-                .stream()
-                .map(BorrowHistoryMapper::toDto)
-                .collect(Collectors.toList());
-    }
-
-    @PostMapping("users/me/borrow-histories/{historyId}/hide")
-    public void hideMyHistory(@PathVariable Long historyId){
-        borrowHistoryService.hideHistoryById(historyId, UsernameAndIdPrincipal.getIdFromSecurityCtx());
-    }
-
-    @PostMapping("users/me/borrow-histories/{historyId}/unhide")
-    public void unhideMyHistory(@PathVariable Long historyId){
-        borrowHistoryService.unHideHistoryById(historyId, UsernameAndIdPrincipal.getIdFromSecurityCtx());
-    }
-
-    @PostMapping("users/me/change-password")
-    public void changePassword(HttpServletResponse response, PasswordChangeRequest passwordChangeRequest){
-        authService.changePassword(
-                UsernameAndIdPrincipal.getIdFromSecurityCtx(),
-                passwordChangeRequest,
-                response);
-    }
-
-
-    //STAFF and ADMIN
     @GetMapping("users")
     public List<UserDto> getAllUsers(){
         return userService.getAllUsers().stream().map(userMapper::toDto).collect(Collectors.toList());
@@ -108,6 +54,7 @@ public class UserController {
                 .map(BorrowHistoryMapper::toDto)
                 .collect(Collectors.toList());
     }
+
     @PutMapping("users/{id}")
     public void editUser(@PathVariable Long id, @RequestBody UserEditAdminRequest userEditAdminRequest){
         userService.updateUser(userMapper.toEntity(id, userEditAdminRequest));
---
+++ src/main/java/com/Library/restAPI/controller/UserMeController.java
@@ -0,0 +1,80 @@
+package com.Library.restAPI.controller;
+
+import com.Library.restAPI.dto.request.PasswordChangeRequest;
+import com.Library.restAPI.dto.request.UserEditRequest;
+import com.Library.restAPI.dto.response.BorrowDto;
+import com.Library.restAPI.dto.response.BorrowHistoryDto;
+import com.Library.restAPI.dto.response.UserDto;
+import com.Library.restAPI.mapper.BorrowHistoryMapper;
+import com.Library.restAPI.mapper.BorrowMapper;
+import com.Library.restAPI.mapper.UserMapper;
+import com.Library.restAPI.security.UsernameAndIdPrincipal;
+import com.Library.restAPI.service.AuthService;
+import com.Library.restAPI.service.BorrowHistoryService;
+import com.Library.restAPI.service.BorrowService;
+import com.Library.restAPI.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/")
+public class UserMeController {
+
+    private final UserService userService;
+    private final BorrowService borrowService;
+    private final BorrowHistoryService borrowHistoryService;
+    private final AuthService authService;
+    private final UserMapper userMapper;
+    private final BorrowMapper borrowMapper;
+
+    @GetMapping("users/me")
+    public UserDto getMe(){
+        return userMapper.toDto(userService.getUserById(UsernameAndIdPrincipal.getIdFromSecurityCtx()));
+    }
+
+    @PutMapping("users/me")
+    public void editMe(@RequestBody UserEditRequest userEditRequest){
+        userService.updateUser(userMapper.toEntity(UsernameAndIdPrincipal.getIdFromSecurityCtx(), userEditRequest));
+    }
+
+    @GetMapping("users/me/borrows")
+    public List<BorrowDto> getMyBorrows(){
+        return borrowService
+                .getAllBorrowsByUserId(UsernameAndIdPrincipal.getIdFromSecurityCtx())
+                .stream()
+                .map(borrowMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("users/me/borrow-histories")
+    public List<BorrowHistoryDto> getMyHistory(@RequestParam(required = false) boolean showHidden){
+        return borrowHistoryService
+                .getAllHistoryByUserId(UsernameAndIdPrincipal.getIdFromSecurityCtx(),showHidden)
+                .stream()
+                .map(BorrowHistoryMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("users/me/borrow-histories/{historyId}/hide")
+    public void hideMyHistory(@PathVariable Long historyId){
+        borrowHistoryService.hideHistoryById(historyId, UsernameAndIdPrincipal.getIdFromSecurityCtx());
+    }
+
+    @PostMapping("users/me/borrow-histories/{historyId}/unhide")
+    public void unhideMyHistory(@PathVariable Long historyId){
+        borrowHistoryService.unHideHistoryById(historyId, UsernameAndIdPrincipal.getIdFromSecurityCtx());
+    }
+
+    @PostMapping("users/me/change-password")
+    public void changePassword(HttpServletResponse response, PasswordChangeRequest passwordChangeRequest){
+        authService.changePassword(
+                UsernameAndIdPrincipal.getIdFromSecurityCtx(),
+                passwordChangeRequest,
+                response);
+    }
+}
